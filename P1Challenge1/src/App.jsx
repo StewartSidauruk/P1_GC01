f6c5ed952217a2db1dcbc59d7f35b1395035b579
StewartSidauruk <220711816@students.uajy.ac.id>
2025-06-20 21:51:32 +0700

    izin tepar karena tugas kuliah

--- P1Challenge1/src/App.jsx
+++ P1Challenge1/src/App.jsx
@@ -95,6 +95,7 @@ function App() {
                   <MenuItem>
                     <a href="#" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
                       Sign out
+                      Izin lagi tepar karena push tugas kak
                     </a>
                   </MenuItem>
                 </MenuItems>
